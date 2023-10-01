b354cd09f555a5086c74c81cec2369c10d3a6716
PRIYANSHI CHAUDHARY <85169981+ph-22416@users.noreply.github.com>
2023-10-01 20:10:49 +0530

    Create account.java

---
+++ account.java
@@ -0,0 +1,28 @@
+public class account {
+    private String name;
+    private String phoneNumber;
+    private String emailAddress;
+
+    public account(String name, String phoneNumber, String emailAddress) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + ", Phone Number: " + phoneNumber + ", Email: " + emailAddress;
+    }
+}
